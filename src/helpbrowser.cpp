f7e2ac67a1d2c5f6d54700a9fa779ea1fd816176
Jürgen Mülbert <develop@juergen-muelbert.de>
2014-03-09 18:50:10 +0100

    Began Add Help Start with QML Improve the Database, but the DB must generate from Source-Code by QT

---
+++ src/helpbrowser.cpp
@@ -0,0 +1,28 @@
+/*
+ * helpbrowser.cpp - HelpBrowser
+ *
+ * Created by Jürgen Mülbert on 09.03.2014
+ * Copyright (c) 2014 Jürgen Mülbert. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the European Union Public Licence (EUPL),
+ * version 1.1.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ *
+ * You should have received a copy of the European Union Public Licence
+ * along with this program. If not, see
+ * http://www.osor.eu/eupl/european-union-public-licence-eupl-v.1.1
+ *
+ */
+
+
+#include "helpbrowser.h"
+
+HelpBrowser::HelpBrowser(QObject *parent) :
+    QTextBrowser(parent)
+{
+}
