108e988858694ee692b3a3abb2c260c8f82a82e8
Jürgen Mülbert <develop@juergen-muelbert.de>
2018-10-20 10:35:23 +0200

    improve appveyor-build-win.ps1

--- resources/scripts/.appveyor-build-win.ps1
+++ resources/scripts/.appveyor-build-win.ps1
@@ -14,22 +14,24 @@ ls
 #$mak_path = "C:\jmbde-QT\jmbde-build\Makefile"
 $env:PATH = "C:\jmbde-QT\$qt_stub\bin\;" + $env:PATH
 
+if (-not (test-path "C:\jmbde-QT\jmbde.pro")) {throw "C:\jmbde-QT\jmbde.pro needed"}
+
 & "C:\jmbde-QT\$qt_stub\bin\qmake.exe" "C:\jmbde-QT\jmbde.pro" "-r" "CONFIG+=release"
 
 #$mak_contents = [System.IO.File]::ReadAllText($mak_path).Replace(" qjp2.lib", "")
 #[System.IO.File]::WriteAllText($mak_path, $mak_contents)
 
-& "nmake"
+& "jom"
 
 cd "src\jmbde"
 
-& "nmake" install
+& "jom" install
 
 cd "app"
 
 & "C:\jmbde-QT\$qt_stub\bin\windeployqt.exe" --no-translations "jmbde.exe" # "???.dll"
-Copy-Item -Path C:\textosaurus\$qt_stub\bin\*eay* -Destination .\
+Copy-Item -Path C:\jmbde-QT\$qt_stub\bin\*eay* -Destination .\
 
 cd ..
 
-& "nmake" windows_all
+& "jom" windows_all
