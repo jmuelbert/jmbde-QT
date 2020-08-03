2678a35ea3191d190adc7a8b110682948a9a8126
Jürgen Mülbert <juergen.muelbert@gmail.com>
2020-08-03 13:42:10 +0200

    Resolve errors in  build workflow
    Update linux builfiles.

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -29,22 +29,24 @@ if(MSVC)
   set(CPACK_PACKAGE_INSTALL_DIRECTORY "jmbde")
 
   if(BUILD_NSIS)
-    set(CPACK_PACKAGE_ICON
-        "${ICONS_DIR}/${PROJECT_NAME}-icon.ico"")
+    set(CPACK_PACKAGE_ICON "${ICONS_DIR}/${PROJECT_NAME}-icon.ico")
     set(CPACK_GENERATOR "NSIS")
-    set(CPACK_NSIS_MUI_ICON "${ICONS_DIR}/${PROJECT_NAME}-icon.ico"")
+    set(CPACK_NSIS_MUI_ICON
+        "${ICONS_DIR}/${PROJECT_NAME}-icon.ico"")
     set(CPACK_NSIS_MUI_UNIICON
         "${ICONS_DIR}/${PROJECT_NAME}-icon.ico"")
-    set(CPACK_NSIS_DISPLAY_NAME "Qv2ray")
-    set(CPACK_NSIS_PACKAGE_NAME "Qv2ray")
-    set(CPACK_NSIS_EXTRA_PREINSTALL_COMMANDS "
+    set(CPACK_NSIS_DISPLAY_NAME "jmbde")
+    set(CPACK_NSIS_PACKAGE_NAME "jmbde")
+    set(CPACK_NSIS_EXTRA_PREINSTALL_COMMANDS
+        "
         ExecWait
         \\\"taskkill
         /f
         /im
         jmbde.exe\\\"
         ")
-           set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
+    set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS
+        "
         CreateShortCut
         \\\"$DESKTOP\\\\Qv2ray.lnk\\\"
         \\\"$INSTDIR\\\\jmbde.exe\\\"
@@ -54,7 +56,8 @@ if(MSVC)
         \\\"$SMPROGRAMS\\\\$STARTMENU_FOLDER\\\\jmbde\\\\jmbde.lnk\\\"
         \\\"$INSTDIR\\\\jmbde.exe\\\"
         ")
-    set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "
+    set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS
+        "
         ExecWait
         \\\"taskkill
         /f
@@ -94,19 +97,21 @@ if(MSVC)
 endif()
 
 if(APPLE)
-    set(CPACK_GENERATOR "DragNDrop")
-    if(DS_STORE_SCRIPT)
-        set(CPACK_DMG_DS_STORE_SETUP_SCRIPT "${CMAKE_SOURCE_DIR}/cmake/CMakeDMGSetup.scpt")
-    else()
-        set(CPACK_DMG_DS_STORE "${CMAKE_SOURCE_DIR}/assets/DS_Store")
-    endif()
+  set(CPACK_GENERATOR "DragNDrop")
+  if(DS_STORE_SCRIPT)
+    set(CPACK_DMG_DS_STORE_SETUP_SCRIPT
+        "${CMAKE_SOURCE_DIR}/cmake/CMakeDMGSetup.scpt")
+  else()
+    set(CPACK_DMG_DS_STORE "${CMAKE_SOURCE_DIR}/assets/DS_Store")
+  endif()
 
-    set(CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/assets/CMakeDMGBackground.png")
-    configure_file("${CMAKE_SOURCE_DIR}/assets/package_dmg.json.in" "${CMAKE_SOURCE_DIR}/assets/package_dmg.json" @ONLY)
+  set(CPACK_DMG_BACKGROUND_IMAGE
+      "${CMAKE_SOURCE_DIR}/assets/CMakeDMGBackground.png")
+  configure_file("${CMAKE_SOURCE_DIR}/assets/package_dmg.json.in"
+                 "${CMAKE_SOURCE_DIR}/assets/package_dmg.json" @ONLY)
 endif()
 
 include(CPack)
-
 
 if(UNIX)
   set(CPACK_GENERATOR "TGZ")
@@ -117,15 +122,17 @@ if(UNIX)
     set(CPACK_RPM_PACKAGE_LICENSE "GPLv3+")
     set(CPACK_RPM_PACKAGE_GROUP "Business/Tools")
     set(CPACK_RPM_PACKAGE_VERSION "${JMBDE_VERSION}")
-    set(CPACK_RPM_COMPONENT_INSTALL ON) # necessary even if CPACK_COMPONENT_INSTALL
-                                    # set to ON. A bug in my opinion.
+    set(CPACK_RPM_COMPONENT_INSTALL ON) # necessary even if
+                                        # CPACK_COMPONENT_INSTALL
+    # set to ON. A bug in my opinion.
   elseif(DEB)
     # Debian specific configuration (minimum)
     # _____________________________________________________________________________
     # CPACK: DEB Specific Settings
     set(CPACK_DEBIAN_PACKAGE_SECTION "Business")
     set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_CONTACT}")
-    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS
+        "libc6
             (>= 2.3.1-6)
             ,
             libboost-test-dev")
@@ -134,6 +141,7 @@ if(UNIX)
 endif()
 
 if(IFW)
+
 endif()
 
 # Directories to look for dependencies
@@ -141,9 +149,9 @@ set(DIRS "${CMAKE_BINARY_DIR}")
 
 # Path used for searching by FIND_XXX(), with appropriate suffixes added
 if(CMAKE_PREFIX_PATH)
-    foreach(dir ${CMAKE_PREFIX_PATH})
-        list(APPEND DIRS "${dir}/bin" "${dir}/lib")
-    endforeach()
+  foreach(dir ${CMAKE_PREFIX_PATH})
+    list(APPEND DIRS "${dir}/bin" "${dir}/lib")
+  endforeach()
 endif()
 
 # Append Qt's lib folder which is two levels above Qt5Widgets_DIR
@@ -152,7 +160,7 @@ list(APPEND DIRS "/usr/local/lib")
 list(APPEND DIRS "/usr/lib")
 
 if(MSVC)
-    set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION .)
+  set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION .)
 endif()
 
 include(InstallRequiredSystemLibraries)
@@ -161,7 +169,9 @@ message(STATUS "APPS: ${APPS}")
 message(STATUS "QT_PLUGINS: ${QT_PLUGINS}")
 message(STATUS "DIRS: ${DIRS}")
 
-install(CODE "
+install(
+  CODE "
    include(BundleUtilities)
    fixup_bundle(\"${APPS}\"   \"\"   \"${DIRS}\")
-   " COMPONENT Runtime)
+   "
+  COMPONENT Runtime)
