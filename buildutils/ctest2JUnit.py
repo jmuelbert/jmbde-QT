f68c829fbbae54d39feb039906784cad95339882
Jürgen Mülbert <jmuelbert@users.noreply.github.com>
2020-12-26 15:49:23 +0100

    Update ctest2JUnit.py

--- buildutils/ctest2JUnit.py
+++ buildutils/ctest2JUnit.py
@@ -1,1 +1,17 @@
 
+from lxml import etree
+import io
+import sys
+
+TAGfile = open(sys.argv[1]+"/Testing/TAG", 'r')
+dirname = TAGfile.readline().strip()
+
+xslfile = open(sys.argv[2], 'r')
+xslcontent = xslfile.read()
+
+xmldoc = etree.parse(sys.argv[1]+"/Testing/"+dirname+"/Test.xml")
+xslt_root = etree.XML(xslcontent)
+transform = etree.XSLT(xslt_root)
+
+result_tree = transform(xmldoc)
+print(result_tree)
