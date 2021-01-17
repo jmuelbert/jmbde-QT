4ba1f7016ec78248c18b4949fd03811323111224
Jürgen <juergen.muelbert@gmail.com>
2021-01-17 11:12:12 +0100

    upgrade ci-build-test and appveyor.

--- cmake/msvc-cpack.cmake
+++ cmake/msvc-cpack.cmake
@@ -10,32 +10,32 @@ message(STATUS "################################################################
 message(STATUS "## CMAKE_CXX_COMPILER_VERSION: " ${CMAKE_CXX_COMPILER_VERSION})
 message(STATUS "## CRTDIR: " ${CRTDIR})
 
-file(GLOB files "${CRTDIR}/*.dll")
-foreach(file ${files})
-  file(TO_CMAKE_PATH ${file} cmake_file)
-  message(STATUS "## ${cmake_file} --> ${QTPLATZ_QT5_RUNTIME_INSTALL_DIRECTORY}")
-  install(
-    PROGRAMS "${cmake_file}"
-    DESTINATION ${QTPLATZ_QT5_RUNTIME_INSTALL_DIRECTORY}
-    COMPONENT applications
-  )
-endforeach()
+# file(GLOB files "${CRTDIR}/*.dll")
+# foreach(file ${files})
+#  file(TO_CMAKE_PATH ${file} cmake_file)
+#  message(STATUS "## ${cmake_file} --> ${QTPLATZ_QT5_RUNTIME_INSTALL_DIRECTORY}")
+#  install(
+#    PROGRAMS "${cmake_file}"
+#    DESTINATION ${QTPLATZ_QT5_RUNTIME_INSTALL_DIRECTORY}
+#    COMPONENT applications
+#  )
+# endforeach()
 
 message(STATUS "###################################################################")
 
-set(REDIST "$ENV{VCTOOLSREDISTDIR}vc_redist.x64.exe")
-message(STATUS ${REDIST})
+# set(REDIST "$ENV{VCTOOLSREDISTDIR}vc_redist.x64.exe")
+# message(STATUS ${REDIST})
 
-if(EXISTS ${REDIST})
-  message(STATUS "VC Redistribution installer found: " ${REDIST})
-  file(TO_CMAKE_PATH ${REDIST} cmake_REDIST)
-  install(
-    PROGRAMS ${cmake_REDIST}
-    DESTINATION ${QTPLATZ_QT5_RUNTIME_INSTALL_DIRECTORY}
-    COMPONENT applications
-  )
-else()
-  message(FATAL "VC Redist installer NOT found")
-endif()
+# if(EXISTS ${REDIST})
+#  message(STATUS "VC Redistribution installer found: " ${REDIST})
+#  file(TO_CMAKE_PATH ${REDIST} cmake_REDIST)
+#  install(
+#    PROGRAMS ${cmake_REDIST}
+#    DESTINATION ${QTPLATZ_QT5_RUNTIME_INSTALL_DIRECTORY}
+#    COMPONENT applications
+#  )
+# else()
+#  message(FATAL "VC Redist installer NOT found")
+# endif()
 
 message(STATUS "###################################################################")
