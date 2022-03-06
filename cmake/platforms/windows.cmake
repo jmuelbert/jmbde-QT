201b8859fa6c374d8b7c3026707afff53fd7a90d
Jürgen <juergen.muelbert@gmail.com>
2022-03-06 13:23:58 +0100

    fix: issues in CMakeLists

--- cmake/platforms/windows.cmake
+++ cmake/platforms/windows.cmake
@@ -6,10 +6,10 @@
 configure_file(${CMAKE_SOURCE_DIR}/assets/app.rc.in
 ${PROJECT_BINARY_DIR}/app.rc @ONLY)
 
-configure_file(assets/installer_Qt6_x64.iss.in
+configure_file(${CMAKE_SOURCE_DIR}/assets/installer_Qt6_x64.iss.in
 ${PROJECT_BINARY_DIR}/installer_Qt6_x64.iss @ONLY)
 
 install(FILES ${PROJECT_BINARY_DIR}/installer_Qt6_x64.iss
 DESTINATION ${CMAKE_INSTALL_BINDIR})
 
-install(TARGETS ${PROJECT_NAME} ${INSTALL_TARGETS_DEFAULT_ARGS})
+install(TARGETS ${TARGET_NAME} ${INSTALL_TARGETS_DEFAULT_ARGS})
