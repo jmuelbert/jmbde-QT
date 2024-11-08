61f034b94f7d3982987f557e5782c8e42e6ad904
Jürgen Mülbert <juergen.muelbert@outlook.de>
2024-11-08 12:41:09 +0100

    chore: Add update nix direnv workflow

--- flake.nix
+++ flake.nix
@@ -34,63 +34,63 @@
     , pre-commit-hooks
     , treefmt-nix
     ,
-    };
+    }:
     flake-utils.lib.eachDefaultSystem (
-    system:
-    let
-      overlays = [ ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      pre-commit = pre-commit-hooks.lib.${system}.run (
-        import ./pre-commit-hooks.nix { inherit pkgs treefmtEval; }
-      );
-      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt-nix;
-    in
-    with pkgs;
-    {
-      apps = {
-        inherit (pre-commit) shellHook;
-        buildInputs = with pkgs; [
-          microsoft-gsl
-          qt6.qtbase
-          qt6.wayland
-          ut
-        ];
-        nativeBuildInputs =
-          [
-            appstream
-            appstream-glib
-            asciidoctor
-            ccache
-            clang
-            clang-tools
-            cmake
-            desktop-file-utils
-            fish
-            flatpak-builder
-            gdb
-            include-what-you-use
-            just
-            lonv
-            librsvg
-            lldb
-            llvm
-            lychee
-            mold-wrapped
-            nil
-            ninja
-            nushell
-            python311Packages.lcov-cobertura
-            qt6.wrapQtAppsHook
-            treefmtEcal.config.build.wrapper
-            (builtins.attrValues treefmtEval.config.build.programs)
-          ]
-          ++ pre-commit.enabledPackages;
-      };
-      formatter = treefmtEval.config.build.wrapper;
-      packages.default = qt6Packages.callPackage ./package.nix { };
-    }
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        pre-commit = pre-commit-hooks.lib.${system}.run (
+          import ./pre-commit-hooks.nix { inherit pkgs treefmtEval; }
+        );
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt-nix;
+      in
+      with pkgs;
+      {
+        apps = {
+          inherit (pre-commit) shellHook;
+          buildInputs = with pkgs; [
+            microsoft-gsl
+            qt6.qtbase
+            qt6.wayland
+            ut
+          ];
+          nativeBuildInputs =
+            [
+              appstream
+              appstream-glib
+              asciidoctor
+              ccache
+              clang
+              clang-tools
+              cmake
+              desktop-file-utils
+              fish
+              flatpak-builder
+              gdb
+              include-what-you-use
+              just
+              lonv
+              librsvg
+              lldb
+              llvm
+              lychee
+              mold-wrapped
+              nil
+              ninja
+              nushell
+              python311Packages.lcov-cobertura
+              qt6.wrapQtAppsHook
+              treefmtEcal.config.build.wrapper
+              (builtins.attrValues treefmtEval.config.build.programs)
+            ]
+            ++ pre-commit.enabledPackages;
+        };
+        formatter = treefmtEval.config.build.wrapper;
+        packages.default = qt6Packages.callPackage ./package.nix { };
+      }
 
     );
 }
